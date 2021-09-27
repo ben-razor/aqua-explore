4971e48aa7fe90eea515dbf07cc0d7cadb1ba48c
Ben Razor <chrisbenwar1@googlemail.com>
2021-09-27 15:41:47 +0100

    Adding Firebase hosting and configuring production build

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -27,7 +27,12 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                use: ['style-loader', {
+                    loader: 'css-loader',
+                    options: {
+                        url: false
+                    }
+                }]
             },
             {
                 test: /\.wasm$/,
